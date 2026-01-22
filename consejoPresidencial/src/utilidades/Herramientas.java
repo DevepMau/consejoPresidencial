00949cbb569cfb3af09f028fcb5a6c48231335d9
DevepMau <oshiettecs@gmail.com>
2026-01-22 18:46:19 -0300

    Elimina GestorDeEventos y mueve la carga de eventos JSON a Herramientas
    - Elimina la clase GestorDeEventos del paquete principal
    - Añade método cargarJSonEvento a utilidades.Herramientas
    - Integra Gson y dependencias de Evento en Herramientas
    - Mantiene funcionalidad de carga de eventos desde archivo JSON
    - Refactoriza para centralizar utilidades en Herramientas

--- consejoPresidencial/src/utilidades/Herramientas.java
+++ consejoPresidencial/src/utilidades/Herramientas.java
@@ -2,8 +2,15 @@ package utilidades;
 
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
+import java.io.FileReader;
+
+import com.google.gson.Gson;
+
+import eventos.Evento;
 
 public class Herramientas {
+	
+	private static final Gson gson = new Gson();
 
 	public BufferedImage escalarImagen(BufferedImage original, int ancho, int alto) {
 
@@ -15,5 +22,15 @@ public class Herramientas {
 		return imagenEscalada;
 
 	}
+	
+	public static Evento cargarJSonEvento(String path) {
+		
+        try (FileReader reader = new FileReader(path)) {
+            return gson.fromJson(reader, Evento.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 
 }
